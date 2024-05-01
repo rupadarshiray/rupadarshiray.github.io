8b6e557bea9eab806af044ebd4f9de7c5d2bb0b4
Rupadarshi ℝay <riddyrayes@gmail.com>
2024-05-01 19:42:58 +0530

    colours and stuff

---
+++ content/academia/math-at-iiserm.md
@@ -0,0 +1,20 @@
+---
+created: 2024-05-01T18:51:59
+modified: 2024-05-01T18:51:59
+tags: 
+aliases: 
+time: Mathematics at IISER Mohali
+---
+
+
+I plan to write about
+
+- the core years of math
+- the major years of math
+- the faculty in math dept
+
+
+Following are few fun articles to read:
+
+- [Explanation of Course Structure at IISERM (2012) - Mast Kalandar (imsc.res.in)](https://www.imsc.res.in/~kapil/blog/iiserm/iiserm_course_structure-2012-12-13-08-16)
+- [Points don't matter - Mast Kalandar (imsc.res.in)](https://www.imsc.res.in/~kapil/blog/education/points-dont-matter-2017-05-16-22-23)
--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     theme: {
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "Latin Modern Sans",
         code: "IBM Plex Mono",
       },
       colors: {
@@ -47,7 +47,7 @@ const config: QuartzConfig = {
       Plugin.FrontMatter(),
       Plugin.TableOfContents(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
+        priority: ["git", "frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
       }),
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
